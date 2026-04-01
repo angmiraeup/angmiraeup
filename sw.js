a3938921b653e204cddb05e914fbe04d8594e386
angmiraeup <azdigitalway@gmail.com>
2026-04-02 00:05:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,5 +8,12 @@ firebase.initializeApp({
     messagingSenderId: "971559169599",
     appId: "1:971559169599:web:082be923ea9782440e128a"
 });
-
 const messaging = firebase.messaging();
+messaging.onBackgroundMessage((p) => {
+    self.registration.showNotification("Mirae Up Support", {
+        body: p.data.body || "New Update!",
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+        vibrate: [500, 100, 500],
+        requireInteraction: true
+    });
+});
