ab8fad3bff98f404ee73fb2cffbd1233a20f9477
angmiraeup <azdigitalway@gmail.com>
2026-04-02 15:49:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
+// 1. OneSignal ki asli power sabse upar (Don't remove this)
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// 2. Tumhara purana Firebase system (Waisa hi rakha hai)
 importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
 
@@ -7,4 +10,15 @@ firebase.initializeApp({
     projectId: "workhatapp",
     messagingSenderId: "971559169599",
     appId: "1:971559169599:web:082be923ea9782440e128a"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((p) => {
+    self.registration.showNotification(p.data.title || "Mirae Support", {
+        body: p.data.body || "New update received",
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+        vibrate: [500, 100, 500],
+        requireInteraction: true
+    });
 });
