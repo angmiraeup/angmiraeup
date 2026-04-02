d687939e921271d4e5bd6acf404ec8f3cf96cf33
angmiraeup <azdigitalway@gmail.com>
2026-04-02 17:57:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
-firebase.initializeApp({apiKey: "AIzaSyDw439IXSmTpustCkIQpRQ7foZ-hc8XiDQ",projectId: "workhatapp",messagingSenderId: "971559169599",appId: "1:971559169599:web:082be923ea9782440e128a"});
+firebase.initializeApp({
+    apiKey: "AIzaSyDw439IXSmTpustCkIQpRQ7foZ-hc8XiDQ",
+    projectId: "workhatapp",
+    messagingSenderId: "971559169599",
+    appId: "1:971559169599:web:082be923ea9782440e128a"
+});
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((p) => {
-    self.registration.showNotification("Mirae Up Support", {
-        body: p.data.body || "New Update!",
+    self.registration.showNotification(p.data.title || "Mirae Support", {
+        body: p.data.body || "New update received",
         icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
         vibrate: [500, 100, 500],
         requireInteraction: true
